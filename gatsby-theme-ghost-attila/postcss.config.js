4cf92e2490f5e6d901bf847c2c01763a223b202a
Shyam <shyamlohar1996@gmail.com>
2020-05-02 22:28:45 +0530

    addded new tailwind config and added new richtext styling with new fonts"

--- gatsby-theme-ghost-attila/postcss.config.js
+++ gatsby-theme-ghost-attila/postcss.config.js
@@ -1,6 +1,13 @@
+const path = require("path");
+
 module.exports = () => ({
   plugins: [
-    require("tailwindcss"),
+    require("tailwindcss")(
+      path.join(
+        process.cwd(),
+        "../gatsby-theme-ghost-attila/tailwind.config.js"
+      )
+    ),
     require("autoprefixer"),
   ],
 });
