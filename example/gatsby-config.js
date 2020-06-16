d31855fd74917dbc68758695e68c6303d3665db6
Keyur <keyur.raval18@gmail.com>
2020-06-16 18:35:16 +0530

    Minor config changes

--- example/gatsby-config.js
+++ example/gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
       resolve: `@draftbox-co/gatsby-ghost-balsa-theme`,
       options: {
         siteConfig: {
-          siteUrl: `https://ghost.theasdfghjkl.com`, // Site domain. Do not include a trailing slash!
+          siteUrl: `https://ghost-balsa-preview.draftbox.co`, // Site domain. Do not include a trailing slash!
 
           postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
@@ -19,50 +19,58 @@ module.exports = {
           backgroundColor: `#e9e9e9`, // Used for Offline Manifest
           themeColor: `#15171A`, // Used for Offline Manifest
           apiUrl: "https://ghost.theasdfghjkl.com",
-          subscribeWidget: {
-            title: "Subscribe to Built with Draftbox",
-            helpText: "Get the latest posts delivered right to your inbox.",
-            successMessage: "Thanks for subscribing to Built with Draftbox.",
-          },
-          footer: {
-            copyright: "Built with Draftbox",
-            navigation: [
-              {
-                label: "Home",
-                url: "https://ghost.theasdfghjkl.com/",
-              },
-              {
-                label: "Sitemap",
-                url: "https://ghost.theasdfghjkl.com/sitemap.xml",
-              },
-              {
-                label: "RSS",
-                url: "https://ghost.theasdfghjkl.com/rss.xml",
-              },
-              {
-                label: "Contact",
-                url: "https://ghost.theasdfghjkl.com/contact",
-              }
-            ],
-          },
           header: {
             navigation: [
               {
                 label: "Home",
-                url: "https://ghost.theasdfghjkl.com/",
+                url: "https://ghost-balsa-preview.draftbox.co/",
               },
               {
                 label: "Contact",
-                url: "https://ghost.theasdfghjkl.com/contact",
+                url: "https://ghost-balsa-preview.draftbox.co/contact",
               },
             ],
           },
+          footer: {
+            copyright: "Built with Draftbox122",
+            navigation: [
+              {
+                label: "Home",
+                url: "https://ghost-balsa-preview.draftbox.co/",
+              },
+              {
+                label: "Sitemap",
+                url: "https://ghost-balsa-preview.draftbox.co/sitemap.xml",
+              },
+              {
+                label: "RSS",
+                url: "https://ghost-balsa-preview.draftbox.co/rss",
+              },
+              {
+                label: "Contact",
+                url: "https://ghost-balsa-preview.draftbox.co/contact",
+              },
+              {
+                label: "External Link",
+                url: "https://spectrum.chat/gatsby-js/themes?tab=posts",
+              },
+            ],
+          },
+          subscribeWidget: {
+            title: "Subscribe to Draftbox [new]",
+            helpText: "Get the latest posts delivered right to your inbox.",
+            successMessage: "Thanks for subscribing to Built with Draftbox.",
+          },
           socialLinks: {
-            twitter: "https://twitter.com/draftboxhq/",
-            facebook: "https://www.facebook.com/draftboxhq/",
-            instagram: "https://www.instagram.com/draftboxhq/",
-            linkedin: "https://linkedin.com/draftboxhq/",
-            github: "https://github.com/draftbox-co/",
+            twitter: "https://twitter.com/draftboxhq",
+            facebook: "https://facebook.com/",
+            instagram: "https://www.instagram.com/",
+            linkedin: "https://linkedin.com",
+            github: "https://github.com/draftbox-co",
+          },
+          contactWidget: {
+            title: "Contact Built with Draftbox",
+            successMessage: "We’ll get in touch with you soon.",
           },
         },
         ghostConfig: {
