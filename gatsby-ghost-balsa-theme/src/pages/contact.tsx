697e13d8e8b9f1cf1ec1b8a72f92fb083ac5053d
Arthur Zapparoli <arthurgeek@users.noreply.github.com>
2020-07-12 13:58:15 +0200

    Adds Metadata for local pages

--- gatsby-ghost-balsa-theme/src/pages/contact.tsx
+++ gatsby-ghost-balsa-theme/src/pages/contact.tsx
@@ -1,10 +1,30 @@
 import React from "react";
 import ContactForm from "../components/ContactForm";
+import MetaData from "../components/meta/MetaData";
+import { useStaticQuery, graphql } from "gatsby";
 import Layout from "../components/Layout";
 
-const Contact = () => {
+const Contact = ({ location }) => {
+  const {
+    site: {
+      siteMetadata: { contactWidget },
+    },
+  } = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          contactWidget {
+            title
+          }
+        }
+      }
+    }
+  `);
+
   return (
     <Layout>
+      <MetaData title={contactWidget.title} location={location} />
+
       <ContactForm />
     </Layout>
   );
